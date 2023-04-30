8b89f361d7314eb1d7cbb12c95dc52cfd2fef46f
Attaftazayn <123521976+Attaftazayn@users.noreply.github.com>
2023-04-30 19:45:54 +0700

    Add some file

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
 const tanah = document.querySelectorAll('.tanah');
 const tikus = document.querySelectorAll('.tikus');
+const levels = document.querySelector(".container1");
 const papanSkor = document.querySelector('.papan-skor');
 const pop = document.querySelector('#pop');
 
 let tanahSebelumnya;
-let selesai;
-let skor;
+let selesai = false;
+let skor = 0;
+
+function difficultyLevel() {
+  const ele = document.getElementsByName("level");
+  for (let i = 0; i < ele.length; i++) {
+    if (ele[i].checked) {
+      return ele[i].id;
+    }
+  }
+}
 
 function randomTanah(tanah) {
   const t = Math.floor(Math.random() * tanah.length);
@@ -23,7 +33,18 @@ function randomWaktu(min, max) {
 
 function munculkanTikus() {
   const tRandom = randomTanah(tanah);
-  const wRandom = randomWaktu(101, 1000);
+  const wRandom = randomWaktu(101, 1000)
+    const difficulty = difficultyLevel();
+  if (difficulty === "easy") {
+    show = 500;
+    hide = 1500;
+  } else if (difficulty === "medium") {
+    show = 200;
+    hide = 1000;
+  } else {
+    show = 100;
+    hide = 800;
+  }
   tRandom.classList.add('muncul');
 
   setTimeout(() => {
@@ -44,10 +65,12 @@ function mulai() {
   }, 15000);
 }
 
-function pukul() {
+function pukul(e) {
+    if (!e.isTrusted) {
+    return;
+  }
   skor++;
-  this.classList.remove('muncul');
-  this.style.transition="0.2s";
+  this.parentNode.classList.remove('muncul');
   pop.play ();
   papanSkor.textContent = skor;
 }
